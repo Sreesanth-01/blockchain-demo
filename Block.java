a5b3af5b05ee72940dfd2f15531a8b14eb11d162
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-02 17:38:42 +0530

    feat: calculateHash function

--- Block.java
+++ Block.java
@@ -3,4 +3,18 @@ public class Block {
     String data;
     String hash;
     String previousHash;
+
+    public Block(int blockNumber, String data, String previousHash){
+        this.blockNumber = blockNumber;
+        this.data = data;
+        this.previousHash = previousHash;
+
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash(){
+        String info = blockNumber+data+previousHash;
+
+        return sha256(info);
+    }
 }
