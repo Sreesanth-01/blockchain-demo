1fa2c00ef2afada1a136f2edb1201c0b029ff310
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-02 17:55:27 +0530

    feat: SHA

--- Block.java
+++ Block.java
@@ -1,3 +1,5 @@
+import java.security.MessageDigest;
+
 public class Block {
     int blockNumber;
     String data;
@@ -16,5 +18,21 @@ public class Block {
         String info = blockNumber+data+previousHash;
 
         return sha256(info);
+    }
+
+    public String sha256(String input){
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = md.digest(input.getBytes());
+            StringBuilder result = new StringBuilder();
+
+            for(byte b : bytes){
+                result.append(String.format("%02x", b));
+            }
+            return result.toString();
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
