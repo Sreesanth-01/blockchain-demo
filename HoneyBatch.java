39e04352a637894db905835706db09ea04f66fa8
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-05 08:36:54 +0530

    feat: HoneyBatch class

---
+++ HoneyBatch.java
@@ -0,0 +1,16 @@
+public class HoneyBatch {
+    String batchId;
+    String beekeeperName;
+    String location;
+    String honeyType;
+    double quantity;
+
+    public HoneyBatch(String batchId, String beekeeperName, String location, String honeyType, double quantity){
+        this.batchId = batchId;
+        this.beekeeperName = beekeeperName;
+        this.location = location;
+        this.honeyType = honeyType;
+        this.quantity = quantity;
+
+    }
+}
