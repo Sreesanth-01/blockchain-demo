8ed5bcb80ffc2ecc9583797720435ececb35c8fa
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-06 12:21:28 +0530

    feat: Digital signature working

--- DigitalSignature.java
+++ DigitalSignature.java
@@ -4,6 +4,7 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.util.Base64;
 
 public class DigitalSignature {
      public static KeyPair generateKeyPair(){
@@ -46,6 +47,39 @@ public class DigitalSignature {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args){
+        KeyPair keys = generateKeyPair();
+
+        PrivateKey privateKey = keys.getPrivate();
+        PublicKey publicKey = keys.getPublic();
+
+        String event = "001 | HARVESTED | 25kg";
+
+        //signing:
+        byte[] digitalSignature = sign(event,privateKey);
+        //Converting to readable text:
+        String signatureText = Base64.getEncoder().encodeToString(digitalSignature);
+
+        System.out.println("Honey event: " + event);
+
+        System.out.println();
+
+        System.out.println("Digital Signature: "+signatureText);
+
+        System.out.println();
+
+        boolean valid = verifySign(event, digitalSignature, publicKey);
+
+        System.out.println("Signature valid: "+valid);
+
+        //Changing the event:
+        String changedEvent = "001 | HARVESTED | 50 kg";
+
+        boolean changedValid = verifySign(changedEvent, digitalSignature, publicKey);
+
+         System.out.println("Signature valid after change: "+ changedValid);
     }
 
 }
