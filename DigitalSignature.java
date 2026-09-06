eca2b6bca4a2ccee4da0b5d604a97c945368bf17
Sreesanth-01 <iamshanth7@gmail.com>
2026-09-06 10:47:17 +0530

    feat: Digital Signature-sign

---
+++ DigitalSignature.java
@@ -0,0 +1,34 @@
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.Signature;
+
+public class DigitalSignature {
+     public static KeyPair generateKeyPair(){
+        try{
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
+            generator.initialize(256);
+
+            return generator.generateKeyPair();
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] sign(String data,PrivateKey privateKey){
+        try {
+            Signature signature = Signature.getInstance("SHA256withECDSA");
+
+            signature.initSign(privateKey);
+
+            signature.update(data.getBytes(StandardCharsets.UTF_8));
+
+            return signature.sign();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
